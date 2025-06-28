dcfeac525f0c5a3c5656c57acf3b7793abe0ad85
Islam Elbanna <islam.s.elbanna@gmail.com>
2025-06-28 19:58:13 +0100

    Clean ups

--- background.js
+++ background.js
@@ -1,250 +1,205 @@
 /*
  * Application types
  */
-//cPanel's text editor
-let TYPE_CPANEL = 0;
-//Everything else
-let TYPE_NORMAL = 1;
+// cPanel's text editor
+const TYPE_CPANEL = 0;
+// Everything else
+const TYPE_NORMAL = 1;
 
-let aceModes = ["actionscript", "groovy", "matlab", "properties", "sql", "c_cpp", "html", "mysql", "protobuf", "sqlserver", "clojure", "java", "nginx", "puppet", "swift", "csharp", "javascript", "objectivec", "python", "text", "css", "json5", "pascal", "r", "typescript", "csv", "json", "perl", "ruby", "vue", "dockerfile", "jsp", "pgsql", "rust", "xml", "dot", "kotlin", "php", "scala", "yaml", "gitignore", "makefile", "plain_text", "sh", "golang", "markdown", "powershell", "soy_template"];
-let aceThemes = ["chrome", "dracula", "github", "sqlserver", "xcode", "clouds", "eclipse", "github_light_default", "terminal", "dawn", "github_dark", "one_dark", "twilight",];
-let defaultLanguage = "java";
-let defaultTheme = "github";
-let defaultWordWrapping = false;
+const aceModes = [
+    "actionscript", "groovy", "matlab", "properties", "sql", "c_cpp", "html", "mysql", "protobuf", "sqlserver",
+    "clojure", "java", "nginx", "puppet", "swift", "csharp", "javascript", "objectivec", "python", "text", "css",
+    "json5", "pascal", "r", "typescript", "csv", "json", "perl", "ruby", "vue", "dockerfile", "jsp", "pgsql", "rust",
+    "xml", "dot", "kotlin", "php", "scala", "yaml", "gitignore", "makefile", "plain_text", "sh", "golang", "markdown",
+    "powershell", "soy_template"
+];
+const aceThemes = [
+    "chrome", "dracula", "github", "sqlserver", "xcode", "clouds", "eclipse", "github_light_default", "terminal",
+    "dawn", "github_dark", "one_dark", "twilight"
+];
+const defaultLanguage = "java";
+const defaultTheme = "github";
+const defaultWordWrapping = false;
+
 let aceItcontextMenuID = null;
-let aceModesFirstLetterContextmenuIDs = [];
+const aceModesFirstLetterContextmenuIDs = {};
 let fieldID = "";
 
 createAceItContextMenu();
 createModesContextMenu();
 createThemesContextMenu();
-createPreferencesContextMenu();
 
-function aceIt(tabID, fieldID)
-{
-    var language = defaultLanguage;
-    var theme = defaultTheme;
-    var wordWrapping = defaultWordWrapping;
-    
-    chrome.storage.local.get(["lastUsedLanguage", "lastUsedTheme", "wordWrapping"], function(items)
-    {
-        if(items.lastUsedLanguage !== undefined)
-        {
+function aceIt(tabID, fieldID) {
+    let language = defaultLanguage;
+    let theme = defaultTheme;
+    let wordWrapping = defaultWordWrapping;
+
+    chrome.storage.local.get(["lastUsedLanguage", "lastUsedTheme", "wordWrapping"], function (items) {
+        if (items.lastUsedLanguage !== undefined) {
             language = items.lastUsedLanguage;
         }
-    
-        if(items.lastUsedTheme !== undefined)
-        {
+
+        if (items.lastUsedTheme !== undefined) {
             theme = items.lastUsedTheme;
         }
-    
-        if(items.wordWrapping !== undefined)
-        {
+
+        if (items.wordWrapping !== undefined) {
             wordWrapping = items.wordWrapping;
         }
-    
-        sendMessage(tabID, {"ace": "it", "language": language, "theme": theme, "wordWrapping": wordWrapping, "elementID": fieldID});
+
+        sendMessage(tabID, {
+            ace: "it",
+            language: language,
+            theme: theme,
+            wordWrapping: wordWrapping,
+            elementID: fieldID
+        });
     });
 }
 
-function sendMessage(tabID, properties)
-{
+function sendMessage(tabID, properties) {
     chrome.tabs.sendMessage(tabID, properties);
 }
 
-function changeLanguage (tabID, languageName)
-{
-    chrome.storage.local.set({lastUsedLanguage: languageName});
-    sendMessage(tabID, {"changeMode":languageName});
-};
-
-function changeTheme (tabID, themeName)
-{
-    chrome.storage.local.set({lastUsedTheme: themeName});
-    sendMessage(tabID, {changeTheme: themeName});
+function changeLanguage(tabID, languageName) {
+    chrome.storage.local.set({ lastUsedLanguage: languageName });
+    sendMessage(tabID, { changeMode: languageName });
 }
 
-function toggleWordWrapping(tabID)
-{
-    var currentValue = defaultWordWrapping;
-    chrome.storage.local.get("wordWrapping", function(items)
-    {
-        if(items.wordWrapping !== undefined)
-        {
+function changeTheme(tabID, themeName) {
+    chrome.storage.local.set({ lastUsedTheme: themeName });
+    sendMessage(tabID, { changeTheme: themeName });
+}
+
+function toggleWordWrapping(tabID) {
+    let currentValue = defaultWordWrapping;
+    chrome.storage.local.get("wordWrapping", function (items) {
+        if (items.wordWrapping !== undefined) {
             currentValue = items.wordWrapping;
         }
 
-        var wordWrapping = !currentValue;
-        chrome.storage.local.set({wordWrapping: wordWrapping});
-        sendMessage(tabID, {toggleWordWrapping: wordWrapping});
+        const wordWrapping = !currentValue;
+        chrome.storage.local.set({ wordWrapping: wordWrapping });
+        sendMessage(tabID, { toggleWordWrapping: wordWrapping });
     });
 }
 
-function toggleAutoLoad(url)
-{
-    isFieldAutoLoaded(url, function(result, items)
-    {
-        var obj = {};
-        if(items["autoLoadingFields"] !== undefined)
-        {
+function toggleAutoLoad(url) {
+    isFieldAutoLoaded(url, function (result, items) {
+        let obj = {};
+        if (items["autoLoadingFields"] !== undefined) {
             obj = items["autoLoadingFields"];
         }
-        
+
         obj[url] = fieldID;
-        if(result !== false)
-        {
-            obj[url] = false;    
+        if (result !== false) {
+            obj[url] = false;
         }
-        chrome.storage.local.set({autoLoadingFields: obj});
+        chrome.storage.local.set({ autoLoadingFields: obj });
     });
 }
 
-function isFieldAutoLoaded(url, callback)
-{
-    chrome.storage.local.get("autoLoadingFields", function(items)
-    {
-        callback(items["autoLoadingFields"]!== undefined && items["autoLoadingFields"][url] !== undefined && items["autoLoadingFields"][url] !== false, items);
+function isFieldAutoLoaded(url, callback) {
+    chrome.storage.local.get("autoLoadingFields", function (items) {
+        callback(
+            items["autoLoadingFields"] !== undefined &&
+            items["autoLoadingFields"][url] !== undefined &&
+            items["autoLoadingFields"][url] !== false,
+            items
+        );
     });
 }
 
-function createAceItContextMenu()
-{
-    aceItcontextMenuID = chrome.contextMenus.create(
-    {
-        "id": "aceit",
-        "title": "Ace it!",
-        "contexts": ["editable"]
+function createAceItContextMenu() {
+    aceItcontextMenuID = chrome.contextMenus.create({
+        id: "aceit",
+        title: "Ace it!",
+        contexts: ["editable"]
     });
 
-    chrome.runtime.onMessage.addListener(function(message, sender)
-    {
-        if(message.doWhat === "updateState")
-        {
-            var title = "Ace it!";
-            if(message.type === "cpanel")
-            {
+    chrome.runtime.onMessage.addListener(function (message, sender) {
+        if (message.doWhat === "updateState") {
+            let title = "Ace it!";
+            if (message.type === "cpanel") {
                 title = "Ace it! (cPanel detected)";
             }
-        
+
             fieldID = message.elementID;
-            if(fieldID === "")
-            {
+            if (fieldID === "") {
                 fieldID = "_aceAnywhereOrigin";
             }
-        
-            var autoLoadState = false;
-            isFieldAutoLoaded(message.url.href, function(result, items)
-            {
-                autoLoadState = result;
-                chrome.contextMenus.update("autoload", {checked: autoLoadState});
-            });
-            
-            chrome.contextMenus.update(aceItcontextMenuID, {title: title});
-        }
-        else if(message.doWhat === "aceIt")
-        {
 
+            isFieldAutoLoaded(message.url.href, function (result) {
+                chrome.contextMenus.update("autoload", { checked: result });
+            });
+
+            chrome.contextMenus.update(aceItcontextMenuID, { title: title });
+        } else if (message.doWhat === "aceIt") {
             aceIt(sender.tab.id, message.elementID);
         }
     });
 }
 
-function createModesContextMenu()
-{
-    chrome.contextMenus.create(
-    {
-        "id": "acemodes",
-        "title": "Language",
-        "contexts": ["editable"]
+function createModesContextMenu() {
+    chrome.contextMenus.create({
+        id: "acemodes",
+        title: "Language",
+        contexts: ["editable"]
     });
 
-    for(var i=0; i<aceModes.length; i++)
-    {
-        var language = aceModes[i];
-        var firstLetter = language[0];
-        var parentContextMenuID = aceModesFirstLetterContextmenuIDs[firstLetter];
-        if(parentContextMenuID === undefined)
-        {
-            parentContextMenuID = chrome.contextMenus.create(
-            {
-                "id": "first_letter_"+firstLetter,
-                "title": firstLetter.toUpperCase(),
-                "contexts": ["editable"],
-                "parentId": "acemodes"
+    for (let i = 0; i < aceModes.length; i++) {
+        const language = aceModes[i];
+        const firstLetter = language[0];
+        let parentContextMenuID = aceModesFirstLetterContextmenuIDs[firstLetter];
+        if (parentContextMenuID === undefined) {
+            parentContextMenuID = chrome.contextMenus.create({
+                id: "first_letter_" + firstLetter,
+                title: firstLetter.toUpperCase(),
+                contexts: ["editable"],
+                parentId: "acemodes"
             });
             aceModesFirstLetterContextmenuIDs[firstLetter] = parentContextMenuID;
         }
-        chrome.contextMenus.create(
-        {
-            "id": language,
-            "title": language,
-            "contexts": ["editable"],
-            "parentId": parentContextMenuID
+        chrome.contextMenus.create({
+            id: language,
+            title: language,
+            contexts: ["editable"],
+            parentId: parentContextMenuID
         });
     }
 }
-function createThemesContextMenu()
-{
-    chrome.contextMenus.create(
-    {
-        "id": "acethemes",
-        "title": "Themes",
-        "contexts": ["editable"]
+
+function createThemesContextMenu() {
+    chrome.contextMenus.create({
+        id: "acethemes",
+        title: "Themes",
+        contexts: ["editable"]
     });
-    chrome.storage.local.get("lastUsedTheme", function(items)
-    {        
-        for(var i=0; i<aceThemes.length; i++)
-        {
-            var themeName = aceThemes[i];
-            chrome.contextMenus.create(
-            {
-                "id": themeName,
-                "title": themeName,
-                "checked": false,
-                "contexts": ["editable"],
-                "parentId": "acethemes"
+    chrome.storage.local.get("lastUsedTheme", function () {
+        for (let i = 0; i < aceThemes.length; i++) {
+            const themeName = aceThemes[i];
+            chrome.contextMenus.create({
+                id: themeName,
+                title: themeName,
+                checked: false,
+                contexts: ["editable"],
+                parentId: "acethemes"
             });
         }
     });
 }
-function createPreferencesContextMenu()
-{
-    chrome.contextMenus.create(
-    {
-        "id": "preferences",
-        "title": "Preferences",
-        "contexts": ["editable"]
-    });
-    chrome.contextMenus.create(
-    {
-        "id": "wordwrapping",
-        "title": "Toggle Word Wrapping",
-        "contexts": ["editable"],
-        "parentId": "preferences"
-    });
-    chrome.contextMenus.create(
-    {
-        "id": "autoload",
-        "title": "Auto load Ace on this URL",
-        "contexts": ["editable"],
-        "parentId": "preferences",
-        "type": "checkbox"
-    });
-}
 
-// Add this at the end of the file to handle context menu clicks in MV3
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    if(info.menuItemId === "aceit") {
-        chrome.tabs.sendMessage(tab.id, {ace: "it"});
-    } else if(info.menuItemId === "wordwrapping") {
-        chrome.tabs.sendMessage(tab.id, {toggleWordWrapping: true});
-    } else if(info.menuItemId === "autoload") {
-        chrome.tabs.sendMessage(tab.id, {toggleAutoLoad: true, url: info.pageUrl});
-    } else if(info.menuItemId === "acemodes" || info.menuItemId.startsWith("first_letter_")) {
+// Handle context menu clicks in MV3
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    if (info.menuItemId === "aceit") {
+        chrome.tabs.sendMessage(tab.id, { ace: "it" });
+    } else if (info.menuItemId === "wordwrapping") {
+        chrome.tabs.sendMessage(tab.id, { toggleWordWrapping: true });
+    } else if (info.menuItemId === "acemodes" || info.menuItemId.startsWith("first_letter_")) {
         // Do nothing for parent menu
-    } else if(aceModes.includes(info.menuItemId)) {
-        chrome.tabs.sendMessage(tab.id, {changeMode: info.menuItemId});
-    } else if(aceThemes.includes(info.menuItemId)) {
-        chrome.tabs.sendMessage(tab.id, {changeTheme: info.menuItemId});
+    } else if (aceModes.includes(info.menuItemId)) {
+        chrome.tabs.sendMessage(tab.id, { changeMode: info.menuItemId });
+    } else if (aceThemes.includes(info.menuItemId)) {
+        chrome.tabs.sendMessage(tab.id, { changeTheme: info.menuItemId });
     }
 });
--- content.js
+++ content.js
@@ -1,209 +1,113 @@
 let containers = {};
-// Keyboard shortcut: Ctrl + Shift + E
+
+// Keyboard shortcut: Ctrl + Shift + U
 document.addEventListener('keydown', (e) => {
-  if (e.ctrlKey && e.shiftKey && e.code === 'KeyU') {
-    let focusedEle = document.activeElement;
-    let container = containers[focusedEle.parentElement.id];
-    if (container) {
-      let editor = container.editor;
-      let originalEle = document.getElementById(container.originalElement);
-      focusedEle.parentElement.remove();
-      originalEle.style.display = 'block';
-      editor.destroy();
-    } else if (focusedEle.tagName === 'TEXTAREA') {
-      activateAceEditor(focusedEle);
+    if (e.ctrlKey && e.shiftKey && e.code === 'KeyU') {
+        handleAceForActiveElement();
     }
-  }
 });
 
+function handleAceForActiveElement() {
+    const focusedEle = document.activeElement;
+    const container = containers[focusedEle.parentElement.id];
+    if (container) {
+        const editor = container.editor;
+        const originalEle = document.getElementById(container.originalElement);
+        focusedEle.parentElement.remove();
+        originalEle.style.display = 'block';
+        editor.destroy();
+    } else if (focusedEle.tagName === 'TEXTAREA') {
+        activateAceEditor(focusedEle);
+    }
+}
+
 function loadAce(callback) {
-  if (window.ace) {
-    callback();
-    return;
-  }
-  loadFile('ace/ace.js', () => {
-    // Load mode and theme
-    loadFile('ace/ext-language_tools.js');
-    loadFile('ace/ext-inline_autocomplete.js');
-    loadFile(`ace/mode-javascript.js`);
-    loadFile('ace/theme-github.js', callback);
-  });
+    if (window.ace) {
+        callback();
+        return;
+    }
+    loadFile('ace/ace.js', () => {
+        loadFile('ace/ext-language_tools.js');
+        loadFile('ace/ext-inline_autocomplete.js');
+        loadFile('ace/mode-javascript.js');
+        loadFile('ace/theme-github.js', callback);
+    });
 }
 
 function activateAceEditor(textarea) {
-  loadAce(() => {
-    const value = textarea.value;
-    const parent = textarea.parentElement;
-    const editorDiv = document.createElement('div');
+    loadAce(() => {
+        const value = textarea.value;
+        const parent = textarea.parentElement;
+        const editorDiv = document.createElement('div');
 
-    editorDiv.style.width = textarea.offsetWidth + 'px';
-    editorDiv.style.height = textarea.offsetHeight + 'px';
-    editorDiv.style.border = '1px solid #ccc';
+        editorDiv.style.width = textarea.offsetWidth + 'px';
+        editorDiv.style.height = textarea.offsetHeight + 'px';
+        editorDiv.style.border = '1px solid #ccc';
 
-    textarea.style.display = 'none';
-    if(textarea.id === "") {
-      textarea.id = `ace-editor-${Date.now()}`;
-    }
-    editorDiv.id = `ace-editor-${textarea.id}`;
-    parent.insertBefore(editorDiv, textarea);
+        textarea.style.display = 'none';
+        if (textarea.id === "") {
+            textarea.id = `ace-editor-${Date.now()}`;
+        }
+        editorDiv.id = `ace-editor-${textarea.id}`;
+        parent.insertBefore(editorDiv, textarea);
 
-    ace.config.set('basePath', chrome.runtime.getURL('ace'));
-    ace.require(["ace/ace", "ace/ext/language_tools", "ace/ext/inline_autocomplete"], function(ace) {
-      editor = ace.edit(editorDiv);
-      editor.session.setMode("ace/mode/java");
-      editor.setTheme("ace/theme/github");
-      // enable inline autocompletion
-      editor.setOptions({
-          enableBasicAutocompletion: true,
-          enableInlineAutocompletion: true,
-          enableSnippets: true,
-          enableLiveAutocompletion: true,
-          autoScrollEditorIntoView: true,
-      });
-      editor.session.setUseWorker(true);
-      editor.session.addMarker(editor.selection.toOrientedRange(), "ace_selected_word", "text");
-      editor.session.on('change', () => {
-        textarea.value = editor.getValue();
-        textarea.textContent = textarea.value;
-      });
-      editor.setValue(value, -1); // -1 to not move cursor
-      containers[editorDiv.id] = {
-        editor: editor,
-        originalElement: textarea.id
-      };
-    });  
-  });
+        ace.config.set('basePath', chrome.runtime.getURL('ace'));
+        ace.require(["ace/ace", "ace/ext/language_tools", "ace/ext/inline_autocomplete"], function (ace) {
+            const editor = ace.edit(editorDiv);
+            editor.session.setMode("ace/mode/javascript");
+            editor.setTheme("ace/theme/github");
+            editor.setOptions({
+                enableBasicAutocompletion: true,
+                enableInlineAutocompletion: true,
+                enableSnippets: true,
+                enableLiveAutocompletion: true,
+                autoScrollEditorIntoView: true,
+            });
+            editor.session.setUseWorker(true);
+            editor.session.addMarker(editor.selection.toOrientedRange(), "ace_selected_word", "text");
+            editor.session.on('change', () => {
+                textarea.value = editor.getValue();
+                textarea.textContent = textarea.value;
+            });
+            editor.setValue(value, -1); // -1 to not move cursor
+            containers[editorDiv.id] = {
+                editor: editor,
+                originalElement: textarea.id
+            };
+        });
+    });
 }
 
 function loadFile(filePath, callback) {
-  if (filePath) {
-    ace.require(["ace/ace"], () => {
-      const script = document.createElement('script');
-      script.src = chrome.runtime.getURL(filePath);
-      script.onload = () => {
-        if(callback) {
-          callback();
-        }
-      }
-      document.head.appendChild(script);
-    });
-  }
+    if (filePath) {
+        ace.require(["ace/ace"], () => {
+            const script = document.createElement('script');
+            script.src = chrome.runtime.getURL(filePath);
+            script.onload = () => {
+                if (callback) {
+                    callback();
+                }
+            };
+            document.head.appendChild(script);
+        });
+    }
 }
 
-//The context menu was clicked
-chrome.runtime.onMessage.addListener(function(message)
-{
-    var code = "";
-    if(message.changeMode !== undefined)
-    {
-      let focusedEle = document.activeElement;
-      let container = containers[focusedEle.parentElement.id];
-      if (container) {
-        let editor = container.editor;
+// The context menu was clicked
+chrome.runtime.onMessage.addListener(function (message) {
+    const focusedEle = document.activeElement;
+    const container = containers[focusedEle.parentElement.id];
+    if (!container) {
+        return; // No active Ace editor
+    }
+    const editor = container.editor;
+    if (message.changeMode !== undefined) {
         editor.session.setMode(`ace/mode/${message.changeMode}`);
-      }
-    }
-    else if(message.changeTheme !== undefined)
-    {
-        if(editorNode !== null)
-        {
-            aceEditor.setTheme(message.changeTheme);
-        }
-    }
-    else if(message.toggleWordWrapping !== undefined)
-    {
-        if(editorNode !== null)
-        {
-            aceEditor.setWordWrapping(message.toggleWordWrapping)
-        }
-    }
-    else
-    {
-        // if(editableArea === null)
-        // {
-        //     editableArea = document.getElementById(message.elementID);
-        // }
-
-        // //Assign ID to the original editable area if there's none
-        // if(editableArea.id === "")
-        // {
-        //     editableArea.id = "_aceAnywhereOrigin";
-        // }
-
-        // //Create a wrapper for the Ace Editor
-        // editorNode = document.createElement("div");
-        // //Insert it just above the original editable area
-        // editableArea.parentNode.insertBefore(editorNode, editableArea);
-        // //Give it an ID
-        // editorNode.setAttribute("id", "_aceAnywhereEditor");
-        // //Set inital content from the original editable area
-        // editorNode.innerHTML = editableArea.value.escapeHTMLEntities();
-        // //Same height
-        // editorNode.style.height = editableArea.offsetHeight+"px";
-        // //Same width
-        // editorNode.style.width = editableArea.offsetWidth+"px";
-        // //Hide the original editable area
-        // editableArea.style.display = "none";
-
-        // //Import Ace from a CDN
-        // var aceJS = document.createElement("script");
-        // //aceJS.src = "//cdnjs.cloudflare.com/ajax/libs/ace/1.1.2/ace.js";
-        // aceJS.src = "//cdnjs.cloudflare.com/ajax/libs/ace/1.1.3/ace.js";
-        // aceJS.setAttribute("charset", "utf-8");
-        // //When the script it finally loaded
-        // aceJS.onload = function()
-        // {
-        //     var aceExtLanguageTools = document.createElement("script");
-        //     aceExtLanguageTools.src = "//cdnjs.cloudflare.com/ajax/libs/ace/1.1.3/ext-language_tools.js";
-        //     aceExtLanguageTools.onload = function()
-        //     {
-        //         document.head.appendChild(aceExtLanguageTools);
-        //         /*
-        //          * 1. Initialize Ace editor
-        //          * 
-        //          * 2. If TYPE_NORMAL keep changes on the original editable area,
-        //          * so that changes can be saved by the application
-        //          * 
-        //          * 3. If TYPE_CPANEL, copy contents to the "CODEWINDOW.value" variable
-        //          * only when the save button is pressed.
-        //          */
-        //         var loadAce = document.createElement("script");
-        //         loadAce.innerHTML =
-        //         '\
-        //             var editableArea = document.getElementById("'+editableArea.id+'")\n\
-        //             ace.require("ace/ext/language_tools");\n\
-        //             var editor = ace.edit("_aceAnywhereEditor");\n\
-        //             editor.setOptions(\n\
-        //             {\n\
-        //                 enableBasicAutocompletion: true\n\
-        //             });\n\
-        //             editor.getSession().setUseWorker(false);\n\
-        //             editor.getSession().on("change", function(e)\n\
-        //             {\n\
-        //                 if(typeof CODEWINDOW === "undefined")\n\
-        //                 {\n\
-        //                     editableArea.innerHTML = editor.getSession().getValue();\n\
-        //                 }\n\
-        //             });\n\
-        //             var sform_submit = document.getElementById("sform_submit");\n\
-        //             if(sform_submit !== null)\n\
-        //             {\n\
-        //                 sform_submit.onclick = function()\n\
-        //                 {\n\
-        //                     CODEWINDOW.value = editor.getSession().getValue();\n\
-        //                 }\n\
-        //             }\n\
-        //         ';
-
-        //         document.head.appendChild(loadAce);
-        //         aceEditor.setLanguage(message.language);
-        //         aceEditor.setTheme(message.theme);
-        //         aceEditor.setWordWrapping(message.wordWrapping);
-        //     };
-        
-        //     document.head.appendChild(aceExtLanguageTools);
-        // };
-
-        // document.head.appendChild(aceJS);
+    } else if (message.changeTheme !== undefined) {
+        editor.setTheme(`ace/theme/${message.changeTheme}`);
+    } else if (message.toggleWordWrapping !== undefined) {
+        editor.setWordWrapping(message.toggleWordWrapping);
+    } else if (message.ace === "it") {
+        handleAceForActiveElement();
     }
 });
